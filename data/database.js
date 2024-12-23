04c1fb47c4685ebaffff258de498c3e1a0c688af
SongHyojun0228 <thdgywns2300@gmail.com>
2024-12-23 16:11:05 +0900

    save

--- data/database.js
+++ data/database.js
@@ -5,17 +5,17 @@ const MongoClient = mongodb.MongoClient;
 let database;
 
 async function connect() {
-  try {
-    const client = await MongoClient.connect(
-      "mongodb+srv://thdgywns2300:5w23otvzi7MsCLtT@clothes.7lk32.mongodb.net/?retryWrites=true&w=majority&appName=Clothes"
-    );
-    database = client.db("Denim");
-    console.log("MongoDB Atlas 연결 성공");
-  } catch (error) {
-    console.error("MongoDB Atlas 연결 실패:", error);
-    throw error;
-  }
-}
+    try {
+        const client = await MongoClient.connect(
+            'mongodb+srv://thdgywns2300:oF4luy5LHKI7Cah3@gym.4vl2x.mongodb.net/Gym?retryWrites=true&w=majority&appName=Gym'
+        );
+        database = client.db('Gym'); 
+        console.log('MongoDB Atlas 연결 성공');
+    } catch (error) {
+        console.error('MongoDB Atlas 연결 실패:', error);
+        throw error;
+    }
+}  
 
 function getDb() {
   if (!database) {
