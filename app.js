1bcc07742012a2dc3ea1103500a01e4cc04685b4
SongHyojun0228 <thdgywns2300@gmail.com>
2024-12-25 17:01:40 +0900

    new and update

--- app.js
+++ app.js
@@ -1,37 +1,62 @@
-const express = require('express');
-
+const express = require("express");
 const app = express();
 
-const path = require('path');
+const db = require("./data/database");
+const session = require("express-session");
+const mongodbStore = require("connect-mongodb-session");
+const MongoDbStore = mongodbStore(session);
 
-const defaultRouter = require('./routes/route'); 
-const userRouter = require('./routes/user'); 
-const addRouter = require('./routes/add'); 
-const wishRouter = require('./routes/wish'); 
+const sessionStore = new MongoDbStore({
+  uri: "mongodb+srv://thdgywns2300:oF4luy5LHKI7Cah3@gym.4vl2x.mongodb.net/Gym?retryWrites=true&w=majority&appName=Gym",
+  databaseName: "Clothes",
+  collection: "sessions",
+});
 
-const db = require('./data/database');
+app.use(
+  session({
+    secret: "super-secret",
+    resave: false,
+    saveUninitialized: false,
+    store: sessionStore,
+    cookie: {
+      maxAge: 2 * 60 * 60 * 1000,
+      httpOnly: true,
+    },
+  })
+);
 
-app.set('view engine', 'ejs');
+app.use((req, res, next) => {
+  res.locals.session = req.session;
+  next();
+});
 
-app.set('views', path.join(__dirname, 'views'));
+const path = require("path");
+const defaultRouter = require("./routes/route");
+const userRouter = require("./routes/user");
+const addRouter = require("./routes/add");
+const wishRouter = require("./routes/wish");
 
-app.use(express.static('public'));
+app.set("view engine", "ejs");
+
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 
-app.use('/', defaultRouter);
-app.use('/', userRouter);
-app.use('/', addRouter);
-app.use('/', wishRouter); 
+app.use("/", defaultRouter);
+app.use("/", userRouter);
+app.use("/", addRouter);
+app.use("/", wishRouter);
 
 app.use((req, res) => {
-    res.status(404).render('404');
+  res.status(404).render("404");
 });
 
 app.use((err, req, res, next) => {
-    console.error(err.stack); 
-    res.status(500).render('500');
+  console.error(err.stack);
+  res.status(500).render("500");
 });
 
 db.connectToDatabase().then(function () {
-    app.listen(3000);
+  app.listen(3000);
 });
--- routes/user.js
+++ routes/user.js
@@ -1,33 +1,137 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const bcrypt = require('bcrypt');
-const db = require('../data/database');
+const bcrypt = require("bcrypt");
+const db = require("../data/database");
 
-router.get('/sign-up', function(req, res) {
-    res.render('sign-up');
+router.get("/sign-up", function (req, res) {
+  res.render("sign-up", { error: {} });
 });
 
-router.post('/sign-up', async function(req,res) {
-    const enteredEmail = req.body.email;
-    const enteredPassword = req.body.password;
-    const enteredConfirmPassword = req.body.confirmpassword;
-    const enteredName = req.body.name;
-    const enteredNickName = req.body.nickname;
+router.post("/sign-up", async function (req, res) {
+  const enteredEmail = req.body.email;
+  const enteredPassword = req.body.password;
+  const enteredConfirmPassword = req.body.confirmpassword;
+  const enteredName = req.body.name;
+  const enteredNickName = req.body.nickname;
 
-    const hashedPassword = bcrypt.hash(enteredPassword, 12);
+  const error = {};
 
-    const newUser = {
-        email : enteredEmail,
-        password : enteredPassword,
-        name : enteredName,
-        nickname : enteredNickName,
+  const existingUser = await db.getDb().collection("users").findOne({
+    email: enteredEmail,
+  });
+
+  if (existingUser) {
+    console.log(existingUser);
+    error.emailMessage = "해당 이메일의 계정이 존재합니다.";
+  }
+
+  if (!enteredEmail) {
+    error.emailMessage = "이메일(아이디)을 입력하세요.";
+  }
+
+  if (!enteredPassword) {
+    error.passwordMessage = "비밀번호를 입력하세요.";
+  }
+
+  if (enteredPassword != enteredConfirmPassword) {
+    error.confirmpasswordMessage = "비밀번호를 재확인하세요.";
+  }
+
+  if (!enteredName) {
+    error.nameMessage = "이름을 입력하세요.";
+  }
+
+  if (!enteredNickName) {
+    error.nicknameMessage = "닉네임을 입력하세요.";
+  }
+
+  const existingNickname = await db.getDb().collection("users").findOne({
+    nickname: enteredNickName,
+  });
+
+  if (existingNickname) {
+    error.nicknameMessage = "해당 닉네임이 존재합니다.";
+  }
+
+  if (Object.keys(error).length > 0) {
+    return res.render("sign-up", { error });
+  }
+
+  const hashedPassword = await bcrypt.hash(enteredPassword, 12);
+
+  const newUser = {
+    email: enteredEmail,
+    password: hashedPassword,
+    name: enteredName,
+    nickname: enteredNickName,
+  };
+
+  const result = await db.getDb().collection("users").insertOne(newUser);
+  console.log(result);
+
+  res.redirect("/");
+});
+
+router.get("/login", function (req, res) {
+  res.render("login", { error: {} });
+});
+
+router.post("/login", async function (req, res) {
+  const enteredEmail = req.body.email;
+  const enteredPassword = req.body.password;
+
+  const existingUser = await db
+    .getDb()
+    .collection("users")
+    .findOne({ email: enteredEmail });
+
+  const error = {};
+
+  if (!enteredEmail) {
+    error.emailMessage = "이메일(아이디)을 입력하세요.";
+  }
+
+  if (!enteredPassword) {
+    error.passwordMessage = "비밀번호를 입력하세요.";
+  }
+
+  if (!existingUser) {
+    error.emailMessage = "해당 이메일의 계정이 존재하지 않습니다.";
+    return res.render("login", { error });
+  }
+
+  const hashedPasswordEqual = await bcrypt.compare(
+    enteredPassword,
+    existingUser.password
+  );
+
+  if (!hashedPasswordEqual) {
+    error.passwordMessage = "비밀번호가 일치하지 않습니다.";
+    return res.render("login", { error });
+  }
+
+  req.session.user = {
+    id : existingUser._id,
+    email : existingUser.email.toString(),
+    nickname : existingUser.nickname.toString(),
+  };
+
+  req.session.isAuthenticated = true;
+
+  req.session.save(error => {
+    if(error) {
+        console.log("세션 저장 오류 : ", error);
     }
+    console.log("로그인 성공");
+    res.redirect("/");
+  })
+});
 
-    const result = await db.getDb().collection('users').insertOne(newUser);
-    console.log(result);
-
-    res.redirect('/');
+router.post("/logout", function(req,res) {
+    req.session.destroy(() => {
+        res.redirect('/');
+    });
 });
 
 module.exports = router;
